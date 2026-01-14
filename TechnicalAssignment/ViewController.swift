1da9bcb79ea29245f5fed25cd918107151225b30
Oleksandr <lagutin7234@gmail.com>
2026-01-14 15:35:36 +0100

    Initial Commit

---
+++ TechnicalAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalAssignment
+//
+//  Created by Александр Лагутин on 14/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
